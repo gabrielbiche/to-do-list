62f5378c7ca8ebf846d375519a4e9088ede23f15
gabrielsbiche <gabrielsbiche@gmail.com>
2022-08-26 14:25:04 -0300

    added SAS and stylized the APP

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,10 +2,6 @@ import React from "react";
 
 export default class Button extends React.Component {
   render() {
-    return (
-      <button>
-        Button
-      </button>
-    );
+    return <button>Button</button>;
   }
 }
