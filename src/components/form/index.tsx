a44b464193ae8f5bb18d17fc43fa6fbdf030bc34
gabrielsbiche <gabrielsbiche@gmail.com>
2022-08-29 16:24:33 -0300

    added state in form component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,14 +3,24 @@ import Button from "../button";
 import style from "./Form.module.scss";
 
 export default class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00",
+  };
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">To do</label>
           <input
             type="text"
             name="task"
+            onChange={(event) =>
+              this.setState({ ...this.state, task: event.target.value })
+            }
             id="task"
             placeholder="Enter your task"
             required
@@ -21,6 +31,10 @@ export default class Form extends React.Component {
           <input
             type="time"
             name="time"
+            value={this.state.time}
+            onChange={(event) =>
+              this.setState({ ...this.state, time: event.target.value })
+            }
             step="1"
             id="time"
             min="00:00:00"
@@ -28,9 +42,7 @@ export default class Form extends React.Component {
             required
           />
         </div>
-        <Button>
-          Add
-        </Button>
+        <Button>Add</Button>
       </form>
     );
   }
