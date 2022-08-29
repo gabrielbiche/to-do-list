5dcc6a5970d846884b364a9ded67548ba6ccd81f
gabrielsbiche <gabrielsbiche@gmail.com>
2022-08-29 16:31:35 -0300

    added typo to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,10 +3,12 @@ import style from "./Button.module.scss";
 
 export default class Button extends React.Component<{
   children: React.ReactNode;
+  type?: "button" | "submit" | "reset" | undefined;
 }> {
   render() {
-    return( 
-      <button className={style.button}>
+    const { type = "button" } = this.props;
+    return (
+      <button className={style.button} type={type}>
         {this.props.children}
       </button>
     );
