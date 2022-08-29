c12219ee8d9c4cb97026b5cad16382b5e6b9001c
gabrielsbiche <gabrielsbiche@gmail.com>
2022-08-29 16:11:28 -0300

    added state component list

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import style from "./List.module.scss";
 import Item from "./item";
 
 export default function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "test01",
       time: "01:00:00",
@@ -12,11 +12,17 @@ export default function List() {
       task: "test02",
       time: "02:00:00",
     },
-  ];
+  ]);
 
   return (
     <aside className={style.todoList}>
-      <h2>Tasks of the day</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks]);
+        }}
+      >
+        Tasks of the day
+      </h2>
       <ul className={style.item}>
         {tasks.map((item, index) => (
           <Item key={index} {...item} />
