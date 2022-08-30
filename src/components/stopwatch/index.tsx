0c83abda49ab2fac84fa4a25e53ccfa2feeb0e71
gabrielsbiche <gabrielsbiche@gmail.com>
2022-08-30 08:41:35 -0300

    implemented task time conversion and added time to stopwatch

--- src/components/stopwatch/index.tsx
+++ src/components/stopwatch/index.tsx
@@ -1,13 +1,26 @@
 import Clock from "./clock";
 import Button from "../button";
 import style from "./stopwatch.module.scss";
+import { timeToSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
+import { useEffect, useState } from "react";
 
-export default function Stopwatch() {
+interface Props {
+  selected: ITask | undefined;
+}
+
+export default function Stopwatch({ selected }: Props) {
+  const [time, setTime] = useState<number>();
+  useEffect(() => {
+    if (selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected]);
   return (
     <div className={style.stopwatch}>
       <p className={style.title}> choose a card and start the timer </p>
       <div className={style.clockWrapper}>
-        <Clock />
+        <Clock time={time} />
       </div>
       <Button>Init</Button>
     </div>
