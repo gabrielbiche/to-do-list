a853ce8a12ec40510dee90219708f96d7a321a3f
gabrielsbiche <gabrielsbiche@gmail.com>
2022-08-30 10:12:49 -0300

    refactored button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,21 +2,21 @@ import React from "react";
 
 import style from "./Button.module.scss";
 
-export default class Button extends React.Component<{
+interface Props {
   children: React.ReactNode;
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button 
-        className={style.button} 
-        type={type} 
-        onClick={onClick}
-      >
-        {this.props.children}
-      </button>
-    );
-  }
+}
+
+export default function Button({ onClick, type, children }: Props) {
+
+  return (
+    <button 
+      className={style.button} 
+      type={type} 
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
